67c97151a855e05d4bb5d2343a76bbd9ab4ab1b1
Nahiduzzamann <93345313+Nahiduzzamann@users.noreply.github.com>
2023-04-30 09:43:02 +0100

    terms updated

--- src/pages/Login/Register/Register.jsx
+++ src/pages/Login/Register/Register.jsx
@@ -3,10 +3,11 @@ import { useContext } from 'react';
 import { Button, Container, Form } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../../../providers/AuthProvider';
+import { useState } from 'react';
 
 const Register = () => {
     const { createUser } = useContext(AuthContext);
-
+    const [accepted, setAccepted] = useState(false)
     const handleRegister = event => {
         event.preventDefault();
         const form = event.target;
@@ -24,6 +25,9 @@ const Register = () => {
             .catch(error => {
                 console.log(error);
             })
+    }
+    const handleAccepted = event => {
+        setAccepted(event.target.checked)
     }
     return (
         <Container className='w-25 mx-auto'>
@@ -48,12 +52,13 @@ const Register = () => {
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                    <Form.Check 
-                    type="checkbox" 
-                    name="accept" 
-                    label={<>Accept <Link to='/terms'>Terms and Conditions</Link></>} />
+                    <Form.Check
+                        onClick={handleAccepted}
+                        type="checkbox"
+                        name="accept"
+                        label={<>Accept <Link to='/terms'>Terms and Conditions</Link></>} />
                 </Form.Group>
-                <Button variant="primary" type="submit">
+                <Button variant="primary" disabled={!accepted} type="submit">
                     Register
                 </Button>
                 <br />
