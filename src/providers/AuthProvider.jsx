85e54e1a73569338a8fb3e534bc29e8ff52081b0
Nahiduzzamann <93345313+Nahiduzzamann@users.noreply.github.com>
2023-04-30 08:34:33 +0100

    logout done

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,24 +1,40 @@
 import React, { createContext } from 'react';
-import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
 import app from '../firebase/firebase.config';
+import { useEffect } from 'react';
+import { useState } from 'react';
 
 export const AuthContext = createContext(null);
 
 const auth = getAuth(app);
 
-const AuthProvider = ({children}) => {
-    const user = null;
-    
-    const createUser = (email, password) =>{
-        return createUserWithEmailAndPassword(auth,email,password)
+const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null)
+
+    const createUser = (email, password) => {
+        return createUserWithEmailAndPassword(auth, email, password)
     }
-    const signIn = (email, password)=>{
-        return signInWithEmailAndPassword(auth,email,password)
+    const signIn = (email, password) => {
+        return signInWithEmailAndPassword(auth, email, password)
     }
+    const logOut = () => {
+        return signOut(auth)
+    }
+
+    useEffect(() => {
+      const unsubscribe =  onAuthStateChanged(auth, loggedUser => {
+            setUser(loggedUser)
+        })
+        return ()=>{
+            unsubscribe();
+        }
+    }, [])
+
     const authInfo = {
         user,
         createUser,
         signIn,
+        logOut,
     }
 
     return (
