55c87c4de26e8bb163596f1c38bad5fe1ea2ab41
Nahiduzzamann <93345313+Nahiduzzamann@users.noreply.github.com>
2023-04-30 11:43:38 +0100

    update

--- src/pages/Home/NewsCard/NewsCard.jsx
+++ src/pages/Home/NewsCard/NewsCard.jsx
@@ -2,7 +2,8 @@ import moment from 'moment';
 import React from 'react';
 import { Card, Image } from 'react-bootstrap';
 import { FaEye, FaRegBookmark, FaRegStar, FaShareAlt, FaStar } from 'react-icons/fa';
-import Rating from 'react-rating';
+import { Rating } from '@smastrom/react-rating'
+import '@smastrom/react-rating/style.css'
 import { Link } from 'react-router-dom';
 
 const NewsCard = ({ news }) => {
@@ -29,15 +30,9 @@ const NewsCard = ({ news }) => {
                 </Card.Text>
             </Card.Body>
             <Card.Footer className="text-muted d-flex">
-                <div className='flex-grow-1'>
-                    <Rating
-                        placeholderRating={rating.number}
-                        readonly
-                        emptySymbol={<FaRegStar></FaRegStar>}
-                        placeholderSymbol={<FaStar className='text-warning'></FaStar>}
-                        fullSymbol={<FaStar></FaStar>}
-                    ></Rating>
-                    <span> {rating?.number}</span>
+                <div className='flex-grow-1 d-flex align-items-center'>
+                    <Rating style={{ maxWidth: 120 }} value={rating} readOnly/>
+                    <span className='ms-2'> {rating?.number}</span>
                 </div>
                 <div>
                     <FaEye></FaEye> {total_view}
